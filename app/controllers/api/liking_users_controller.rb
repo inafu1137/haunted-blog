3062a7da42e1454ab27f1001f28efd9d65a1a19a
inafu1137 <inafu1137@gmail.com>
2025-06-06 11:59:09 +0900

    ビューで使用しない変数のインスタンス化を差し戻し

--- app/controllers/api/liking_users_controller.rb
+++ app/controllers/api/liking_users_controller.rb
@@ -2,9 +2,9 @@
 
 class Api::LikingUsersController < ApplicationController
   def index
-    @blog = Blog.find(params[:blog_id])
-    @users = @blog.liking_users.order(:id)
-    @my_liking = @blog.likings.find_by(user: current_user)
-    @destroy_path = @my_liking ? api_blog_liking_path(@blog, @my_liking, format: :json) : nil
+    blog = Blog.find(params[:blog_id])
+    @users = blog.liking_users.order(:id)
+    my_liking = blog.likings.find_by(user: current_user)
+    @destroy_path = my_liking ? api_blog_liking_path(blog, my_liking, format: :json) : nil
   end
 end
